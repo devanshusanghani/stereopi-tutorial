a062372b5adb7994d3b7540b453f01359b25f725
Dev Sanghani <dvsangha@mtu.edu>
2024-10-22 08:59:56 -0500

    2_test_script

--- 2_chess_cycle.py
+++ 2_chess_cycle.py
@@ -1,92 +1,76 @@
-# Copyright (C) 2019 Eugene Pomazov, <stereopi.com>, virt2real team
-#
-# This file is part of StereoPi tutorial scripts.
-#
-# StereoPi tutorial is free software: you can redistribute it 
-# and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3 of the 
-# License, or (at your option) any later version.
-#
-# StereoPi tutorial is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with StereoPi tutorial.  
-# If not, see <http://www.gnu.org/licenses/>.
-#
-# Most of this code is updated version of 3dberry.org project by virt2real
-# 
-# Thanks to Adrian and http://pyimagesearch.com, as there are lot of
-# code in this tutorial was taken from his lessons.
-# 
-
 import os
 import time
 from datetime import datetime
-import picamera
-from picamera import PiCamera
+from picamera2 import Picamera2
 import cv2
 import numpy as np
 
 # Photo session settings
 total_photos = 30             # Number of images to take
 countdown = 5                 # Interval for count-down timer, seconds
-font=cv2.FONT_HERSHEY_SIMPLEX # Cowntdown timer font
- 
-# Camera settimgs
-cam_width = 1280              # Cam sensor width settings
-cam_height = 480              # Cam sensor height settings
+font = cv2.FONT_HERSHEY_SIMPLEX # Countdown timer font
 
-# Final image capture settings
-scale_ratio = 0.5
+# Camera settings
+cam_width = 640               # Cam sensor width settings for each camera
+cam_height = 480              # Cam sensor height settings
 
 # Camera resolution height must be dividable by 16, and width by 32
 cam_width = int((cam_width+31)/32)*32
 cam_height = int((cam_height+15)/16)*16
-print ("Used camera resolution: "+str(cam_width)+" x "+str(cam_height))
+print("Used camera resolution: "+str(cam_width)+" x "+str(cam_height))
 
-# Buffer for captured image settings
-img_width = int (cam_width * scale_ratio)
-img_height = int (cam_height * scale_ratio)
-capture = np.zeros((img_height, img_width, 4), dtype=np.uint8)
-print ("Scaled image resolution: "+str(img_width)+" x "+str(img_height))
+# Initialize the cameras
+picam1 = Picamera2(0)  # First camera
+picam2 = Picamera2(1)  # Second camera
 
-# Initialize the camera
-camera = PiCamera(stereo_mode='side-by-side', stereo_decimate=False)
-camera.resolution=(cam_width, cam_height)
-camera.framerate = 20
-camera.hflip = True
+config1 = picam1.create_still_configuration(main={"size": (cam_width, cam_height)})
+config2 = picam2.create_still_configuration(main={"size": (cam_width, cam_height)})
 
-# Lets start taking photos! 
+picam1.configure(config1)
+picam2.configure(config2)
+
+picam1.start()
+picam2.start()
+
+# Allow time for the cameras to warm up
+time.sleep(2)
+
+# Let's start taking photos!
 counter = 0
 t2 = datetime.now()
-print ("Starting photo sequence")
-for frame in camera.capture_continuous(capture, format="bgra", \
-                  use_video_port=True, resize=(img_width,img_height)):
+print("Starting photo sequence")
+
+while counter < total_photos:
+    frame1 = picam1.capture_array("main")
+    frame2 = picam2.capture_array("main")
+    
+    # Combine frames side by side
+    combined_frame = np.hstack((frame1, frame2))
+    
     t1 = datetime.now()
-    cntdwn_timer = countdown - int ((t1-t2).total_seconds())
-    # If cowntdown is zero - let's record next image
-    if cntdwn_timer == -1:
-      counter += 1
-      filename = './scenes/scene_'+str(img_width)+'x'+str(img_height)+'_'+\
-                  str(counter) + '.png'
-      cv2.imwrite(filename, frame)
-      print (' ['+str(counter)+' of '+str(total_photos)+'] '+filename)
-      t2 = datetime.now()
-      time.sleep(1)
-      cntdwn_timer = 0      # To avoid "-1" timer display 
-      next
-    # Draw cowntdown counter, seconds
-    cv2.putText(frame, str(cntdwn_timer), (50,50), font, 2.0, (0,0,255),4, cv2.LINE_AA)
-    cv2.imshow("pair", frame)
+    cntdwn_timer = countdown - int((t1-t2).total_seconds())
+    
+    # If countdown is zero - let's record next image
+    if cntdwn_timer <= 0:
+        counter += 1
+        filename = f'./scenes/stereo_scene_{cam_width*2}x{cam_height}_{counter}.png'
+        cv2.imwrite(filename, combined_frame)
+        print(f' [{counter} of {total_photos}] {filename}')
+        t2 = datetime.now()
+        time.sleep(1)
+        cntdwn_timer = countdown  # Reset the countdown
+    
+    # Draw countdown counter, seconds
+    cv2.putText(combined_frame, str(max(0, cntdwn_timer)), (50,50), font, 2.0, (0,0,255), 4, cv2.LINE_AA)
+    cv2.imshow("Stereo Pair", combined_frame)
     key = cv2.waitKey(1) & 0xFF
     
     # Press 'Q' key to quit, or wait till all photos are taken
-    if (key == ord("q")) | (counter == total_photos):
-      break
+    if key == ord("q"):
+        break
 
- 
-print ("Photo sequence finished")
- 
+print("Photo sequence finished")
+
+picam1.stop()
+picam2.stop()
+cv2.destroyAllWindows()
